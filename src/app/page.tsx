de4264da47eb53fc7cb2bee4062f8931bec08406
Volkan Kıvanç <volki466volki@gmail.com>
2024-11-11 15:11:17 +0300

    initialize project structure with Next.js, TypeScript, Prisma, MongoDB and Tailwind CSS;

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+import AppBar from "@/components/AppBar";
+import AuthDesign from "@/components/AuthDesign";
+import { auth } from "../../auth";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import ClientComp from "@/components/ClientComp";
+
+export default async function Home() {
+  const session = await auth();
+  if (session) {
+    const user = await db.user.findUnique({
+      where: { email: session?.user?.email! },
+      include: { Inventory: true },
+    });
+    console.log(user, "user");
+    if (user && !user?.isAdmin) {
+      return (
+        <>
+          <AppBar />
+          <ClientComp user={user} />
+        </>
+      );
+    } else if (user && user?.isAdmin) {
+      redirect("/dashboard");
+    }
+  } else {
+    return (
+      <div className="">
+        <AppBar />
+        <AuthDesign />
+      </div>
+    );
+  }
+}
